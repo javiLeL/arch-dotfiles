4db038dbf9f21881324f60a36c1ad3303a617303
JaviLeL <javier212002@gmail.com>
2024-05-18 17:34:37 +0200

    adding nvim dotfiles

---
+++ .config/nvim/lua/core/settings.lua
@@ -0,0 +1,25 @@
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.breakindent = true
+vim.opt.scrolloff = 5
+vim.opt.fileencoding = "utf-8"
+vim.opt.termguicolors = true
+
+vim.opt.relativenumber = false
+vim.opt.number = true
+vim.opt.cursorline = true
+
+vim.opt.hidden = true
