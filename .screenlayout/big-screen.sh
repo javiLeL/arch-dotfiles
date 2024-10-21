2883a739e5fb82d8c220655e29513fc73a72a832
JaviLeL <javier212002@gmail.com>
2024-10-21 18:21:30 +0200

    Mon Oct 21 18:21:30 CEST 2024

---
+++ .screenlayout/big-screen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+bspc monitor eDP-1 -r
+bspc monitor HDMI-1 -d 1 2 3 4 5 6 7 
+xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off
